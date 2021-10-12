9f3e7b5f2dd15cc904ba4c631df691addc232033
Ekaterina96Sergeevna <ekaterinapavlova_96@mail.ru>
2021-10-12 13:07:31 +0300

    Download image

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/details/DetailsFragment.kt
@@ -1,10 +1,15 @@
 package com.codinginflow.imagesearchapp.ui.details
 
+import android.app.DownloadManager
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -14,7 +19,9 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.codinginflow.imagesearchapp.R
+import com.codinginflow.imagesearchapp.api.UnsplashApi.Companion.BASE_URL
 import com.codinginflow.imagesearchapp.databinding.FragmentDetailsBinding
+import java.io.File
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
@@ -37,7 +44,7 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
             Glide.with(this@DetailsFragment)
                 .load(photo.urls.full)
                 .error(R.drawable.ic_error)
-                .listener(object : RequestListener<Drawable>{
+                .listener(object : RequestListener<Drawable> {
                     override fun onLoadFailed(
                         e: GlideException?,
                         model: Any?,
@@ -58,6 +65,7 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
                         progressBar.isVisible = false
                         textViewCreator.isVisible = true
                         textViewDescription.isVisible = photo.description != null
+                        buttonDownloadImage.isVisible = true
                         return false
                     }
                 })
@@ -75,6 +83,30 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
                 }
                 paint.isUnderlineText = true
             }
+
+            buttonDownloadImage.setOnClickListener {
+                DownloadImage(photo.urls.full)
+            }
         }
+    }
+
+    fun DownloadImage(imageUrl: String) {
+
+        val file = File(requireContext().externalMediaDirs.first(), imageUrl)
+
+        val request = DownloadManager.Request(Uri.parse(imageUrl))
+            .setTitle("Image downloaded")
+            .setDescription("Downloading")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+            .setDestinationUri(Uri.fromFile(file))
+            .setAllowedOverMetered(true)
+            .setAllowedOverRoaming(false)
+
+        val downloadManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requireContext().getSystemService(DownloadManager::class.java)
+        } else {
+            error("Build.VERSION.SDK_INT < Build.VERSION_CODES.M")
+        }
+        downloadManager?.enqueue(request)
     }
 }
